c01dc3f2b0367d75274c2b85d2b19fbaf6e799f6
ys05059 <ys05059@gmail.com>
2023-04-01 15:52:58 +0900

    feat : Auth 관련 기본 model 생성

---
+++ app/src/main/java/com/knu/cloud/data/UserStore.kt
@@ -0,0 +1,30 @@
+package com.knu.cloud.data
+
+// 참고코드
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserStore(private val context: Context) {
+    // DataStore은 한 번만 생성하도록  companion object 사용
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("userToken")
+        private val USER_TOKEN_KEY = stringPreferencesKey("user_token")
+    }
+
+    val getAccessToken: Flow<String> = context.dataStore.data.map { preferences ->
+        preferences[USER_TOKEN_KEY] ?: ""
+    }
+
+    suspend fun saveToken(token: String) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_TOKEN_KEY] = token
+        }
+    }
+}
