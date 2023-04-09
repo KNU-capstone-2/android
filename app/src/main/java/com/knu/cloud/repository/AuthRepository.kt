fae85ad89b813f2f6a5afa6a846cbd73ecb5dd1e
ys05059 <ys05059@gmail.com>
2023-04-10 01:53:53 +0900

    fix : merge conflict resolve
    - AuthRemoteDataSource.kt
    - AuthRepository.kt
    반환 type 때문에 발생하는 conflict 해결

--- app/src/main/java/com/knu/cloud/repository/AuthRepository.kt
+++ app/src/main/java/com/knu/cloud/repository/AuthRepository.kt
@@ -1,11 +1,16 @@
 package com.knu.cloud.repository
 
+import com.knu.cloud.data.auth.AuthRemoteDataSource
+import com.knu.cloud.model.auth.LoginRequest
 import com.knu.cloud.model.auth.Token
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 interface AuthRepository {
+    suspend fun signUp(email :String , username : String, password:String): Result<String>
+    suspend fun login(id : String, password : String): Result<String>
 
-    suspend fun getTokenFlow() : Flow<Token>
-    suspend fun saveToken(token: Token)
+    val isSignIn : StateFlow<Boolean>
+    val signInError : StateFlow<String>
 
 }
