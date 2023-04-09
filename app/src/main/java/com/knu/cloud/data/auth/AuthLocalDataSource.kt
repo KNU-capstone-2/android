5344d1c0a4fe9ab15c601aeb6a72aa00c331efa1
ys05059 <ys05059@gmail.com>
2023-04-10 02:32:09 +0900

    refactor : Refactor AuthLocalDataSource - 추후 sessionManager 내부에서 구현 예정

--- app/src/main/java/com/knu/cloud/data/auth/AuthLocalDataSource.kt
+++ app/src/main/java/com/knu/cloud/data/auth/AuthLocalDataSource.kt
@@ -3,11 +3,15 @@ package com.knu.cloud.data.auth
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.knu.cloud.data.auth.AuthLocalDataSource.PreferenceKeys.TOKEN
 import com.knu.cloud.model.auth.LoginRequest
 import com.knu.cloud.model.auth.Token
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 import javax.inject.Inject
 
 class AuthLocalDataSource @Inject constructor(
@@ -17,15 +21,27 @@ class AuthLocalDataSource @Inject constructor(
         val TOKEN = stringPreferencesKey("token")
     }
 
+
     // TODO : 리턴 타입 일치시켜야할듯 Datastore은 반환 값이 기본 Flow형태라
-    suspend fun getTokenFlow(loginRequest: LoginRequest): Flow<Token> {
-        TODO("Not yet implemented")
+    suspend fun getTokenFlow(): Flow<String> {
+        return dataStore.data
+            .catch {exception ->
+                if(exception is IOException){
+                    exception.printStackTrace()
+                    emit(emptyPreferences())
+                }else{
+                    throw exception
+                }
+            }
+            .map { prefs->
+                prefs.asMap().values.toString()
+            }
     }
 
-    suspend fun saveToken(token: Token) {
-        if(token.sessionId.isNotEmpty()){
+    suspend fun saveToken(token: String) {
+        if(token.isNotEmpty()){
             dataStore.edit {prefs ->
-                prefs[TOKEN] = token.sessionId
+                prefs[TOKEN] = token
             }
         }
     }
