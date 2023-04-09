febdf4a25e54cf6c9d533c6bf07fb0317932d32c
ys05059 <ys05059@gmail.com>
2023-04-10 02:24:44 +0900

    refactor : Refactor Retrofit Client and Interceptor for DI - Add SessionManager for DI - Add custom CallAdapter - Update authInterceptor

--- app/src/main/java/com/knu/cloud/di/ConfigModule.kt
+++ app/src/main/java/com/knu/cloud/di/ConfigModule.kt
@@ -1,5 +1,6 @@
 package com.knu.cloud.di
 
+import com.knu.cloud.network.networkResultCallAdapter.NetworkResultCallAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -8,20 +9,21 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ConfigModule {
 
+    private const val BASE_URL = "http://192.168.25.11:8080"
     @Singleton
     @Provides
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
             .client(okHttpClient)
-            .baseUrl("BASE_URL")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(NetworkResultCallAdapterFactory.create())
             .build()
     }
 
@@ -33,14 +35,20 @@ object ConfigModule {
         val logging = HttpLoggingInterceptor().apply {
             this.setLevel(HttpLoggingInterceptor.Level.BODY)
         }
-
         return OkHttpClient.Builder()
-            .connectTimeout(100, TimeUnit.MILLISECONDS)
-            .readTimeout(100, TimeUnit.MILLISECONDS)
-            .writeTimeout(100, TimeUnit.MILLISECONDS)
+//            .connectTimeout(100, TimeUnit.MILLISECONDS)
+//            .readTimeout(100, TimeUnit.MILLISECONDS)
+//            .writeTimeout(100, TimeUnit.MILLISECONDS)
             .addInterceptor(logging)
             .addNetworkInterceptor(authInterceptor)
             .build()
     }
 
+    @Singleton
+    @Provides
+    fun provideSessionManager() :SessionManager{
+        return SessionManager()
+    }
 }
+
+
