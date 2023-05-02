20bab3dd3e2a0cdbdd033c42f82ab7d12fb8d8bb
ys05059 <ys05059@gmail.com>
2023-05-02 19:40:15 +0900

    feat : Navigation Drawer

--- app/src/main/java/com/knu/cloud/ProjectApp.kt
+++ app/src/main/java/com/knu/cloud/ProjectApp.kt
@@ -1,29 +1,30 @@
 package com.knu.cloud
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.dialog
 import com.knu.cloud.components.FABContent
+import com.knu.cloud.components.NavDrawer
 import com.knu.cloud.components.ProjectAppBar
-import com.knu.cloud.components.ProjectBottomBar
-import com.knu.cloud.navigation.MainDestination
-import com.knu.cloud.navigation.authNavGraph
-import com.knu.cloud.navigation.homeNavGraph
+import com.knu.cloud.navigation.*
 import com.knu.cloud.screens.instanceCreate.InstanceCreateScreen
 import com.knu.cloud.screens.splash.ProjectSplashScreen
 import com.knu.cloud.ui.theme.CloudTheme
+import timber.log.Timber
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -32,58 +33,76 @@ fun ProjectApp(appState: ProjectAppState = rememberProjectAppState()) {
         Scaffold(
             scaffoldState = appState.scaffoldState,
             topBar = {
-                if(appState.isHomeScreen){
+                if (appState.isHomeSection.value) {
                     ProjectAppBar(
                         title = "POCKET",
                         path = "프로젝트 / Compute / 대시보드"
                     )
                 }
             },
-            bottomBar = {
-                if(appState.isHomeScreen){
-                    ProjectBottomBar(
-                        sections = appState.bottomBarTabs,
-                        currentRoute = appState.currentRoute!!,
-                        navigateToRoute = appState::navigateToBottomBarRoute
-                    )
-                }
-            },
             floatingActionButton = {
-                if(appState.isInstanceScreen){
+                if (appState.isInstanceScreen) {
                     FABContent {
 //                    showInstanceCreateDialog.value = true
                         appState.navController.navigate(MainDestination.INSTANCE_CREATE_ROUTE)
                     }
                 }
             }
-        ) {innerPaddingModifier ->
-            NavHost( /* Root NavHost */
-                navController = appState.navController,
-                startDestination = MainDestination.SPlASH_ROUTE,
-                modifier = Modifier.padding(innerPaddingModifier)
+        ) { innerPaddingModifier ->
+            Row(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ){
-                composable(route = MainDestination.SPlASH_ROUTE){
-                    ProjectSplashScreen(navController = appState.navController)
+                LaunchedEffect(appState.navController){
+                    appState.addDestinationChangedListener()
                 }
-                homeNavGraph(onInstanceCreateBtnClicked = appState::navigateToInstanceCreate)
-                authNavGraph(
-                    onLoginClicked = appState::navigateToHome,
-                    onSignUpClicked = appState::navigateToSignUp,
-                    onSignUpSubmitClicked = appState::navigateToLogin
-                    /*TODO: 로그인 request 보내기*/
-                )
-
-                dialog(
-                    route = MainDestination.INSTANCE_CREATE_ROUTE,
-                    dialogProperties = DialogProperties(usePlatformDefaultWidth = false)
-                ){
-                    Surface(modifier = Modifier
-                        .width(1200.dp)
-                        .height(800.dp)
-                        .padding(10.dp)
-                        .clip(RoundedCornerShape(30.dp))
-                    ) {
-                        InstanceCreateScreen()
+                if(appState.isHomeSection.value){
+                    Timber.tag("navigation").d("NavDrawer composition 일어남")
+                    NavDrawer(
+                        modifier = Modifier
+                            .weight(0.2f)
+                            .background(colorResource(id = R.color.TopAppBar_background)),
+                        sections = homeSections,
+                        currentRoute = appState.currentRoute ?:ComputeSections.DashBoard.route,
+                        navigateToRoute = appState.navActions::navigateToRoute,
+                    )
+                }
+                NavHost( /* Root NavHost */
+                    navController = appState.navController,
+                    startDestination = MainDestination.SPlASH_ROUTE,
+                    modifier = Modifier
+                        .padding(innerPaddingModifier)
+                        .weight(0.8f)
+                ) {
+                    composable(route = MainDestination.SPlASH_ROUTE) {
+                        ProjectSplashScreen(navController = appState.navController)
+                    }
+                    authNavGraph(
+                        onLoginClicked = appState.navActions::navigateToHome,
+                        onSignUpClicked = appState.navActions::navigateToSignUp,
+                        onSignUpSubmitClicked = appState.navActions::navigateToLogin
+                        /* TODO: 로그인 request 보내기 */
+                    )
+                    homeNavGraph(
+                        navController = appState.navController,
+                        navActions = appState.navActions,
+                        onInstanceCreateBtnClicked = appState.navActions::navigateToInstanceCreate
+                    )
+                    dialog(
+                        route = MainDestination.INSTANCE_CREATE_ROUTE,
+                        dialogProperties = DialogProperties(usePlatformDefaultWidth = false)
+                    ){
+                        Surface(
+                            modifier = Modifier
+                                .width(1200.dp)
+                                .height(800.dp)
+                                .padding(10.dp)
+                                .clip(RoundedCornerShape(30.dp))
+                        ) {
+                            InstanceCreateScreen()
+                        }
                     }
                 }
             }
