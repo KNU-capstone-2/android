473e87d17e4cedee4cb31488c54d10c10fbcbc66
ys05059 <ys05059@gmail.com>
2023-05-25 03:02:44 +0900

    feat : getInstance API Ui layer

--- app/src/main/java/com/knu/cloud/screens/home/instanceDetail/InstanceDetailViewModel.kt
+++ app/src/main/java/com/knu/cloud/screens/home/instanceDetail/InstanceDetailViewModel.kt
@@ -5,36 +5,47 @@ import androidx.lifecycle.viewModelScope
 import com.knu.cloud.model.instance.InstanceData
 import com.knu.cloud.network.RetrofitFailureStateException
 import com.knu.cloud.repository.instance.InstanceRepository
+import com.knu.cloud.screens.home.instance.InstanceUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
+
+data class InstanceDetailUiState(
+    val isLoading : Boolean = false,
+    val instance : InstanceData? = null,
+)
 
 @HiltViewModel
 class InstanceDetailViewModel @Inject constructor (
     private val instanceRepository: InstanceRepository
 ) : ViewModel(){
-    private val _instance = MutableStateFlow<InstanceData?>(null)
-    val instance get() = _instance.asStateFlow()
+
+    private val _uiState = MutableStateFlow(InstanceDetailUiState())
+    val uiState : StateFlow<InstanceDetailUiState> = _uiState.asStateFlow()
 
     fun getInstance(instanceId : String){
-        Timber.tag("InstanceViewModel").d("getInstance($instanceId)")
+        Timber.tag("${this.javaClass.name}_getInstance").d("getInstance($instanceId)")
+        _uiState.update {
+            it.copy(isLoading = true)
+        }
         viewModelScope.launch{
             instanceRepository.getInstance(instanceId)
                 .onSuccess { instanceData ->
-                    if(instanceData != null ){
-                        _instance.value  = instanceData
-                    }else{
-                        _instance.value = null
-                    }
+                        Timber.tag("${this.javaClass.name}_getInstance").d("onSuccess instanceData : $instanceData")
+                        _uiState.update { it.copy(instance = instanceData, isLoading = false) }
                 }.onFailure {
+                    _uiState.update { state ->
+                        state.copy(instance = null, isLoading = false)
+                    }
                     it as RetrofitFailureStateException
                     Timber.tag("${this.javaClass.name}_getInstance")
-                        .e("message :${it.message} , code :${it.code}")
+                        .e("onFailure message :${it.message} , code :${it.code}")
                 }
         }
-        Timber.tag("InstanceViewModel").d("getInstance - instance : ${_instance.value}")
     }
 }
