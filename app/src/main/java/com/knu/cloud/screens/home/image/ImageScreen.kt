a47f26b1a8a076a88ba036b06d42932bb37d9762
ys05059 <ys05059@gmail.com>
2023-05-26 22:09:58 +0900

    feat : Image UI layer

--- app/src/main/java/com/knu/cloud/screens/home/image/ImageScreen.kt
+++ app/src/main/java/com/knu/cloud/screens/home/image/ImageScreen.kt
@@ -1,23 +1,240 @@
 package com.knu.cloud.screens.home.image
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.knu.cloud.R
+import com.knu.cloud.components.CenterLottieLoadingIndicator
+import com.knu.cloud.components.DeleteConfirmDialog
+import com.knu.cloud.components.DeleteResultDialog
+import com.knu.cloud.components.basicTable.*
+import com.knu.cloud.model.instanceCreate.ImageData
+import timber.log.Timber
 
 @Composable
-fun ImageScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(text = "ImageScreen")
+fun ImageScreen(
+    onImageCreateClicked: () -> Unit,
+    onImageDetailClicked: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel : ImageViewModel  = hiltViewModel()
+) {
+    val context = LocalContext.current // Toast 메세지를 위함
+    val uiState by viewModel.uiState.collectAsState()
+    var selectedImage by rememberSaveable {
+        mutableStateOf<ImageData?>(null)
     }
+    var isDeleteConfirmDialogOpen by remember { mutableStateOf(false) }
+
+    if (isDeleteConfirmDialogOpen) {
+        DeleteConfirmDialog(
+            data = "이미지",
+            onDeleteBtnClicked = {
+                viewModel.deleteImages()
+            },
+            onCloseBtnClicked =  {
+                isDeleteConfirmDialogOpen = false
+            }
+        )
+    }
+
+    if (uiState.deleteComplete){
+        DeleteResultDialog(
+            data = "이미지",
+            deleteResult = uiState.deleteResult,
+            onCloseBtnClicked = {
+                viewModel.closeDeleteResultDialog()
+            }
+        )
+    }
+    if (uiState.isLoading) {
+        CenterLottieLoadingIndicator()
+    } else {
+        Column() {
+            ImageBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                totalCnt = uiState.images.size,
+                checkedCnt = uiState.checkedImageIds.size,
+                onLaunchBtnClicked = onImageCreateClicked,
+                onDeleteBtnClicked = {
+                    isDeleteConfirmDialogOpen = true
+                }
+            )
+            Divider(modifier = Modifier.height(1.dp), color = Color.Black)
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+            ) {
+                Column(
+                    modifier = Modifier.weight(.7f)
+                ) {
+                    ImageTable(
+                        modifier = Modifier,
+                        dataList = uiState.images,
+                        checkedImageIds = uiState.checkedImageIds,
+                        onAllChecked = {
+                            viewModel.allImagesCheck(it)
+                        },
+                        onRowChecked = { checked, instanceId ->
+                            Timber.tag("vm_test").d("onRowChecked")
+                            if (checked) {
+                                Timber.tag("vm_test").d("instanceCheck call")
+                                viewModel.imageCheck(instanceId)
+                            } else {
+                                viewModel.imageUncheck(instanceId)
+                            }
+                        },
+                        onRowSelected = { imageId ->
+                            val selectedData = uiState.images.find { it.id == imageId }
+                            selectedImage =
+                                if (selectedImage == selectedData) null else selectedData
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageBar(
+    modifier: Modifier = Modifier,
+    totalCnt : Int,
+    checkedCnt : Int,
+    onLaunchBtnClicked : () -> Unit,
+    onDeleteBtnClicked : () -> Unit
+) {
+    Row(modifier = modifier
+        .fillMaxSize()
+    ) {
+        Box(modifier = Modifier
+            .weight(0.2f)
+            .fillMaxSize(),
+            contentAlignment = Alignment.CenterStart
+        ){
+            Row(verticalAlignment = Alignment.Bottom) {
+                Text("Images",
+                    modifier = Modifier.padding(10.dp),
+                    style = MaterialTheme.typography.h6
+                )
+                Text(text = "Total : $totalCnt  Selected : $checkedCnt",
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
+        OutlinedButton(
+            modifier = Modifier.weight(0.1f),
+            onClick = onLaunchBtnClicked
+        ) {
+            Text(text = "Create Image")
+        }
+        OutlinedButton(
+            modifier = Modifier.weight(0.1f),
+            onClick =  onDeleteBtnClicked
+        ) {
+            Text(text = "Delete Images")
+        }
+    }
+}
+
+@Composable
+fun ImageTable(
+    modifier: Modifier = Modifier,
+    dataList :List<ImageData>,
+    checkedImageIds :List<String>,
+    onAllChecked : (Boolean) -> Unit,
+    onRowChecked : (Boolean, String) -> Unit,
+    onRowSelected : (String) -> Unit
+) {
+    var isAllSelected by rememberSaveable { mutableStateOf(false) }
+    var isHeaderChecked by rememberSaveable { mutableStateOf(false) }
+
+    LaunchedEffect(checkedImageIds) {
+        Timber.tag("uiState")
+            .d("${this.javaClass.name} : uiState.checkedInstanceIds $checkedImageIds")
+        if (checkedImageIds.isEmpty()) {
+            /* to initialize table checkBoxes*/
+            isAllSelected = false
+            isHeaderChecked = true
+        }
+    }
+
+    val columnHeaders =  listOf("Image Name", "Status","Size", "Create Date", "Update Date")
+
+    var columnTypes by remember{ mutableStateOf( listOf(
+        TableColumnType.Text,
+        TableColumnType.ColorBox,
+        TableColumnType.ColorBox,
+        TableColumnType.ColorBox,
+        TableColumnType.ColorBox
+    ))}
+    var columnWeights by rememberSaveable{ mutableStateOf(
+        listOf(.2f,.1f,.1f,.1f,.1f)
+    )}
+    /**
+    *  || Name || Status || Size || CreateDate || UpdateDate
+    * */
+    var rowItems by remember { mutableStateOf(emptyList<TableRowItem>())}
+    rowItems = dataList.map { imageData ->
+        val imageNameCell by mutableStateOf( TableCell(imageData.name ))
+        val imageStatusCell by mutableStateOf(
+            TableCell(imageData.status, colorResource(id = R.color.instance_state_running))
+        )
+        val instanceTypeCell by mutableStateOf( TableCell("${imageData.size} MB") )
+        val createDateCell by mutableStateOf( TableCell( imageData.createdDate))
+        val updateDateCell by mutableStateOf( TableCell( imageData.updateDate))
+        val cellItems = remember { mutableListOf(
+            imageNameCell,imageStatusCell,instanceTypeCell,createDateCell,updateDateCell
+        ) }
+        TableRowItem(
+            rowID = imageData.id,
+            columnTypes = columnTypes,
+            isChecked = imageData.id in checkedImageIds,
+            isSelected = false,
+            cells = cellItems.toList()
+        )
+    }
+
+
+    BasicTable(
+        modifier = modifier,
+        tableHeaderItem = TableHeaderItem(
+            textList = columnHeaders,
+            weightList = columnWeights
+        ),
+        tableRowItems = rowItems,
+        columnTypes = columnTypes,
+        columnWeights = columnWeights,
+        onAllChecked = {
+            isAllSelected = true
+            isHeaderChecked = true
+            onAllChecked(it)
+        },
+        onRowChecked = { checked , rowId ->
+            val rowIdx = rowItems.indexOfFirst { it.rowID == rowId }
+            val updateRowItems = rowItems.toMutableList()
+            updateRowItems[rowIdx] = updateRowItems[rowIdx].copy(isChecked = checked)
+            rowItems = updateRowItems
+            if (isAllSelected){
+                isAllSelected = false
+            }
+            isHeaderChecked = false
+            onRowChecked(checked,rowId)
+        },
+        onRowSelected = onRowSelected,
+    )
 }
---
+++ app/src/main/java/com/knu/cloud/screens/home/image/ImageViewModel.kt
@@ -0,0 +1,140 @@
+package com.knu.cloud.screens.home.image
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.knu.cloud.model.instanceCreate.ImageData
+import com.knu.cloud.network.RetrofitFailureStateException
+import com.knu.cloud.repository.home.image.ImageRepository
+import com.knu.cloud.utils.convertDateFormat
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+
+data class ImageUiState(
+    val isLoading : Boolean = false,
+    val images : List<ImageData> = emptyList(),
+    val checkedImageIds : List<String> = emptyList(),
+    val deleteComplete : Boolean = false,
+    val deleteResult : List<Pair<String,Boolean>> = emptyList()
+)
+
+@HiltViewModel
+class ImageViewModel @Inject constructor (
+    private val imageRepository : ImageRepository
+) : ViewModel(){
+
+    private val _uiState = MutableStateFlow(ImageUiState())
+    val uiState :StateFlow<ImageUiState> = _uiState.asStateFlow()
+    init {
+        getAllImages()
+    }
+
+    /**
+     * Image를 전부 가져옴
+     */
+    private fun getAllImages(){
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true) }
+            imageRepository.getImages()
+                .onSuccess { images ->
+                    if (images != null) {
+                        Timber.tag("vm_test").d("getImages :  $images")
+                        _uiState.update { it.copy(images = convertImageData(images.images), isLoading = false) }
+                    }else{
+                        _uiState.update { it.copy(images = emptyList(), isLoading = false) }
+                    }
+                }.onFailure {
+                    _uiState.update { state ->
+                        state.copy(images = emptyList(), isLoading = false)
+                    }
+                    it as RetrofitFailureStateException
+                    Timber.tag("${this.javaClass.name}_getAllImages")
+                        .e("message :${it.message} , code :${it.code}")
+                }
+        }
+    }
+    /**
+     * 체크된 image들을 삭제함
+    * */
+    fun deleteImages(){
+        viewModelScope.launch {
+            val deleteSuccessList : MutableList<String> = mutableListOf()
+            _uiState.value.checkedImageIds.forEach{ imageId ->
+                imageRepository.deleteImage(imageId)
+                    .onSuccess {
+                        deleteSuccessList.add(imageId)
+                    }.onFailure {
+                        it as RetrofitFailureStateException
+                        Timber.tag("${this.javaClass.name}_getAllimages")
+                            .e("message :${it.message} , code :${it.code}")
+                    }
+            }
+            _uiState.update { state ->
+                state.copy(
+                    images = state.images.filterNot { it.id in deleteSuccessList },                              // 삭제 성공한  리스트에 없는 images
+                    deleteResult = state.checkedImageIds.map { id ->
+                        Pair(id, id in deleteSuccessList)
+                    },
+                    deleteComplete = true
+                )
+            }
+        }
+    }
+
+    fun imageCheck(imageId : String) {
+        if(imageId !in _uiState.value.checkedImageIds){
+            _uiState.update { it.copy(checkedImageIds = it.checkedImageIds + imageId) }
+        }
+        Timber.tag("vm_test").d("imageCheck : checkedImageIds ${uiState.value.checkedImageIds}")
+    }
+    fun imageUncheck(imageId: String) {
+        _uiState.update { state ->
+            state.copy(
+                checkedImageIds = state.checkedImageIds.filterNot { it == imageId }
+            )
+        }
+        Timber.tag("vm_test").d("imageUncheck : checkedImageIds ${uiState.value.checkedImageIds}")
+    }
+
+    fun allImagesCheck(allChecked: Boolean) {
+        if(allChecked) {
+            _uiState.update { state ->
+                state.copy( checkedImageIds = state.images.map { it.id})
+            }
+        }else initializeCheckImageIds()
+        Timber.tag("vm_test").d("allImagesCheck : checkedImageIds ${uiState.value.checkedImageIds}")
+    }
+
+    fun closeDeleteResultDialog(){
+        _uiState.update {
+            it.copy( deleteComplete = false, checkedImageIds = emptyList())
+        }
+    }
+
+    private fun initializeCheckImageIds(){
+        _uiState.update { state ->
+            state.copy(checkedImageIds = emptyList())
+        }
+        Timber.tag("vm_test").d("initializeCheckImageIds : checkedImageIds ${uiState.value.checkedImageIds}")
+    }
+
+    private fun convertImageData(images : List<ImageData>) :List<ImageData>{
+        val convertedImages = mutableListOf<ImageData>()
+        Timber.tag("vm_test").d("convertImageData : images :  $images")
+        images.forEach { image ->
+            Timber.tag("vm_test").d("convertImageData : image :  $image")
+            convertedImages.add(image.copy(
+                createdDate = convertDateFormat(image.createdDate),
+                updateDate = convertDateFormat(image.updateDate))
+            )
+        }
+        return convertedImages.toList()
+    }
+
+}
