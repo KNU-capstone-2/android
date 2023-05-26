d29f96a777d9438a8c3e9c68bcf35e98914b1896
ys05059 <ys05059@gmail.com>
2023-05-26 22:08:21 +0900

    feat : convertDateFormat function

--- app/src/main/java/com/knu/cloud/utils/Utils.kt
+++ app/src/main/java/com/knu/cloud/utils/Utils.kt
@@ -1,1 +1,15 @@
 package com.knu.cloud.utils
+import timber.log.Timber
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+fun convertDateFormat(input: String): String {
+    Timber.tag("convertDateFormat").d("input : $input")
+
+    val inputDateTime = ZonedDateTime.parse(input, DateTimeFormatter.ISO_DATE_TIME)
+    val outputDate = inputDateTime.toLocalDateTime().toLocalDate()
+    val returnDate = outputDate.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))
+
+    Timber.tag("convertDateFormat").d("returnDate : $returnDate")
+    return returnDate
+}
