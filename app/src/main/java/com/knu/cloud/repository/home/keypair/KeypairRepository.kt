a82acd28526500edd413c50c12bdda18f2fb384d
ys05059 <ys05059@gmail.com>
2023-05-27 01:41:06 +0900

    feat : Keypair Repository

---
+++ app/src/main/java/com/knu/cloud/repository/home/keypair/KeypairRepository.kt
@@ -0,0 +1,18 @@
+package com.knu.cloud.repository.home.keypair
+
+import com.knu.cloud.model.instanceCreate.KeypairData
+import com.knu.cloud.model.instanceCreate.KeypairsResponse
+import com.knu.cloud.model.keypair.KeypairCreateRequest
+import com.knu.cloud.model.keypair.KeypairCreateResponse
+
+interface KeypairRepository {
+
+    suspend fun getKeypairs() : Result<KeypairsResponse?>
+
+    suspend fun getKeypair(keypairName : String) : Result<KeypairData?>
+
+    suspend fun createKeypair(keypairCreateRequest: KeypairCreateRequest) : Result<KeypairCreateResponse?>
+
+    suspend fun deleteKeypair(keypairName: String) : Result<String?>
+
+}
---
+++ app/src/main/java/com/knu/cloud/repository/home/keypair/KeypairRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.knu.cloud.repository.home.keypair
+
+import com.knu.cloud.data.home.keypair.KeypairRemoteDataSource
+import com.knu.cloud.model.instanceCreate.KeypairData
+import com.knu.cloud.model.instanceCreate.KeypairsResponse
+import com.knu.cloud.model.keypair.KeypairCreateRequest
+import com.knu.cloud.model.keypair.KeypairCreateResponse
+import com.knu.cloud.network.openstackResponseToResult
+import com.knu.cloud.network.responseToResult
+import javax.inject.Inject
+
+class KeypairRepositoryImpl @Inject constructor(
+    private val remoteDataSource: KeypairRemoteDataSource
+) : KeypairRepository{
+    override suspend fun getKeypairs(): Result<KeypairsResponse?> {
+        return openstackResponseToResult(remoteDataSource.getKeypairs())
+    }
+
+    override suspend fun getKeypair(keypairName: String): Result<KeypairData?> {
+        return responseToResult(remoteDataSource.getKeypair(keypairName))
+    }
+
+    override suspend fun createKeypair(keypairCreateRequest: KeypairCreateRequest): Result<KeypairCreateResponse?> {
+        return responseToResult(remoteDataSource.createKeypair(keypairCreateRequest))
+    }
+
+    override suspend fun deleteKeypair(keypairName: String): Result<String?> {
+        return openstackResponseToResult(remoteDataSource.deleteKeypair(keypairName))
+    }
+}
